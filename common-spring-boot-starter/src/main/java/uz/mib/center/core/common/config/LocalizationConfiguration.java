5cb714be100464cef84654e6b131463ce3a7b003
leeway <abdulatif1707@gmail.com>
2021-06-21 22:58:21 +0500

    0.0.7

--- common-spring-boot-starter/src/main/java/uz/mib/center/core/common/config/LocalizationConfiguration.java
+++ common-spring-boot-starter/src/main/java/uz/mib/center/core/common/config/LocalizationConfiguration.java
@@ -1,11 +1,20 @@
 package uz.mib.center.core.common.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import uz.mib.center.core.common.properties.LocaleProperties;
+
+import java.util.Locale;
 
 @Configuration(proxyBeanMethods = false)
 @EnableConfigurationProperties(LocaleProperties.class)
@@ -20,6 +29,25 @@ public class LocalizationConfiguration {
         source.setBasename(properties.getResourceBaseName());
         source.setUseCodeAsDefaultMessage(true);
         return source;
+    }
+
+
+
+    /**
+     * This interceptor allows visitors to change the locale on a per-request basis
+     *
+     * @return a LocaleChangeInterceptor object
+     */
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        //instantiate the object with an empty constructor
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+
+        //the request param that we'll use to determine the locale
+        interceptor.setParamName(properties.getDefaultParamName());
+
+        //get out
+        return interceptor;
     }
 
 }
--- sample/src/main/java/com/example/sample/controller/TestController.java
+++ sample/src/main/java/com/example/sample/controller/TestController.java
@@ -2,14 +2,17 @@ package com.example.sample.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping
 public class TestController {
 
     @GetMapping
-    public String hello(){
-        return "It is working";
+    public String hello(@RequestParam Map<String, String> param){
+        return param.toString();
     }
 }
